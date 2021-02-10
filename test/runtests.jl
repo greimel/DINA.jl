4a1c9a9a871618c891bc00a0c65c3c53d11c7703
Fabian Greimel <fabgrei@gmail.com>
2021-02-10 22:34:51 +0100

    years

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,6 @@ using Test
     tbl = get_dina(1980)
     @test tbl isa DINA.StatFiles.StatFile
 
-    @test sort(dina_years) == [1962; 1964; 1966:2019]
+    @test sort(dina_years()) == [1962; 1964; 1966:2019]
 
 end
