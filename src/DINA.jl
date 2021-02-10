49d493092d41a47195002c3a2cf012af5ed8056f
Fabian Greimel <fabgrei@gmail.com>
2021-02-10 22:29:49 +0100

    panel

--- src/DINA.jl
+++ src/DINA.jl
@@ -1,11 +1,16 @@
 module DINA
 
-export get_dina, dina_years
+export get_dina, dina_years, aggregate_quantiles, dina_quantile_panel
 
 using DataDeps
 using StatFiles
-using DataFrames: DataFrame
+using TableOperations
+using CategoricalArrays: cut
+using DataFrames: DataFrame, disallowmissing!, groupby, combine
 using ReadableRegex: look_for, one_or_more, DIGIT
+using LinearAlgebra: dot
+using Chain: @chain
+using StatsBase: wquantile
 
 function __init__()
     register(DataDep(
@@ -43,5 +48,40 @@ function get_dina(year)
     file = "USDINA/usdina$(year).dta"
     load(@datadep_str(file))
 end
+
+function dina_quantile_panel(var, byvar, ngroups, years = dina_years())
+    mapreduce(vcat, years) do yr
+        aggregate_quantiles(var, byvar, ngroups, yr)
+    end
+end
+
+function aggregate_quantiles(var, byvar, ngroups, year)
+	tbl0 = get_dina(year)
+	
+	ids = [:id]
+	grp = [:age]
+	wgt = :dweght
+	
+	cols = unique([ids; grp; wgt; var; byvar])
+	
+	tbl = TableOperations.select(tbl0, cols...)
+	
+	df = DataFrame(tbl) |> disallowmissing!
+	
+    q = 0:1/ngroups:1
+    
+	df.group = cut(df[!,byvar], wquantile(df[!,byvar], df[!,wgt], q), extend=true, labels = format)
+	
+	agg_df = combine(
+        groupby(df, [:group, :age]),
+        ([v, wgt] => ((x,w) -> dot(x,w)./sum(w)) => v for v in var)..., wgt => sum => wgt
+        )
+    
+    agg_df[!,:year] .= year
+	
+	agg_df
+end
+
+format(from, to, i; kwargs...) = "group $i"
 
 end
