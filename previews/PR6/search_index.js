9281b1d2754ba00a2adf17cb403e8b34da0ce213
Documenter.jl <documenter@juliadocs.github.io>
2021-02-11 13:58:02 +0000

    build based on adad52f

---
+++ previews/PR6/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DINA","category":"page"},{"location":"#DINA","page":"Home","title":"DINA","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DINA]","category":"page"}]
+}
